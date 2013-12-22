d9523dc66a45d6471b86f7200d3325821facb5c4
gnishida <gnishida@purdue.edu>
2013-12-22 11:33:50 -0500

    Update Sketch
    - draw vertices as well.

--- RoadSketch/Sketch.cpp
+++ RoadSketch/Sketch.cpp
@@ -12,7 +12,7 @@ void Sketch::generateMesh() {
 
 	renderables.clear();
 
-	// road edge
+	// road edges
 	RoadEdgeIter ei, eend;
 	for (boost::tie(ei, eend) = boost::edges(graph); ei != eend; ++ei) {
 		if (!graph[*ei]->valid) continue;
@@ -22,9 +22,32 @@ void Sketch::generateMesh() {
 		QColor color = QColor(0, 0, 255);
 
 		Renderable renderable(GL_LINE_STRIP);
-		addMeshFromEdge(&renderable, edge, widthBase, color, 0.0f);
+		addMeshFromEdge(&renderable, edge, widthBase, color, 10.0f);
 		renderables.push_back(renderable);
 	}
+
+	// road vertices
+	Renderable renderable(GL_POINTS, 10.0f);
+	Vertex v;
+
+	v.color[0] = 0;
+	v.color[1] = 0;
+	v.color[2] = 1.0f;
+	v.color[3] = 1.0f;
+	v.normal[0] = 0.0f;
+	v.normal[1] = 0.0f;
+	v.normal[2] = 1.0f;
+
+	RoadVertexIter vi, vend;
+	for (boost::tie(vi, vend) = boost::vertices(graph); vi != vend; ++vi) {
+		if (!graph[*vi]->valid) continue;
+
+		v.location[0] = graph[*vi]->pt.x();
+		v.location[1] = graph[*vi]->pt.y();
+		v.location[2] = 10.0f;
+		renderable.vertices.push_back(v);
+	}
+	renderables.push_back(renderable);
 
 	modified = false;
 }
