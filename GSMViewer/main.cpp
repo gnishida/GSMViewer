c5db48f4e4ef55240c96891a0a66b3f0f569d583
gnishida <gnishida@purdue.edu>
2013-12-21 21:20:06 -0500

    Add TopNSearch class.

--- GSMViewer/main.cpp
+++ GSMViewer/main.cpp
@@ -1,8 +1,7 @@
 #include "MainWindow.h"
 #include <QtGui/QApplication>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	QApplication a(argc, argv);
 	MainWindow w;
 	w.show();
